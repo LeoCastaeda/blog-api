c3304de3e9a6bb684d6dd990d3df040eeb4854d7
Leo Castañeda <leocc_78@hotmail.com>
2024-11-20 03:08:39 +0100

    updatesv1

---
+++ src/application/dtos/authorize-user.dto.ts
@@ -0,0 +1,14 @@
+import {Role} from "../../domain/entities/Role";
+
+export class AuthorizeUserDto {
+    userId: number;
+    userRole: Role;
+    action: string;
+
+
+constructor(userId: number, userRole: Role, action: string) {
+    this.userId = userId;
+    this.userRole = userRole;
+    this.action = action;
+}
+}
